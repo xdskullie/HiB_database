68bccd4d7dafc2d63708393917c18548c71de4d6
xdskullie <tillymjohansson@gmail.com>
2025-12-03 12:25:13 +0100

    Updatering
    Skapade status klass med konstanter

---
+++ status.js
@@ -0,0 +1,13 @@
+class Status{
+    constructor(label, color){
+        this.label = label;
+        this.color = color;
+    }
+}
+
+const bokad = new Status("Bokad", "green");
+const ejBokad = new Status("Ej bokad", "red");
+const ringa = new Status("Ringa", "yellow");
+const aterkoppla = new Status("Återkoppla", "blue");
+const färdig = new Status("Färdig", "white");
+
